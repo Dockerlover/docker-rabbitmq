6d94dbbf4f98a8aa53eea833e2e51795487c4e2d
刘红 <liuhong1.happy@163.com>
2015-07-09 10:57:11 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,8 @@
 
 set -m
 
-if [ ! -f /.rabbitmq_password_set ]; then
-	/set_rabbitmq_password.sh
-fi
-
 # make rabbit own its own files
-chown -R rabbitmq:rabbitmq /var/lib/rabbitmq
+
 
 if [ -z "$CLUSTER_WITH" ] ; then
     /usr/sbin/rabbitmq-server
